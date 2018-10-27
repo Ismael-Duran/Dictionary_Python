b2fcbfac02be8452e962494da4fcf48471764799
Ismael-Duran <ismael.d.o1@gmail.com>
2018-10-27 12:59:40 -0700

    Include comments and while-loop

--- dictionary_proj.py
+++ dictionary_proj.py
@@ -1,44 +1,48 @@
-
+## Import the following 
 import json, string, difflib
 
+## Download the JSON file on Github called "data.json"
 data = json.load(open('data.json'))
 
+# Function used to search for the word user queries
 def dct(keyWord):
-    keyWord = keyWord.lower()    
-    if keyWord in data:
+    keyWord = keyWord.lower()   ## lowercase all letters in the keyWord
+    
+    if keyWord in data:             ## search for word in data
         return data[keyWord]
-    elif keyWord.title() in data:
+    
+    elif keyWord.title() in data:   ## used string.title() to convert original keyWord and try to search again 
         return data[keyWord.title()]
-    elif keyWord.upper() in data:
+    
+    elif keyWord.upper() in data:       ## used string.upper() to uppercase all letters and search again
         return data[keyWord.upper()]
-    elif len(difflib.get_close_matches(keyWord, data.keys(), cutoff=0.7)) > 0:
-        yn = input( "Did you mean %s? Enter Y if yes or N if no." %(difflib.get_close_matches(keyWord, data.keys(), cutoff=0.7))[0])
-
+    
+    elif len(difflib.get_close_matches(keyWord, data.keys(), cutoff=0.7)) > 0: ##user mistypes keyWord, find approximate in dictionary
+        
+        yn = input( "Did you mean %s? Enter Y if yes or N if no. " %(difflib.get_close_matches(keyWord, data.keys(), cutoff=0.7))[0])
+        
         if yn == "Y":
             return data[difflib.get_close_matches(keyWord, data.keys(), cutoff=0.7)[0]]
         elif yn == "N":
-            return "The word does not exist."
+            return "The word does not exist. And enter another key."
         else:
-            return "We did not understand your entry."
+            return "We did not understand your entry. And enter another key."
     else:
-        return "The word does not exist."
+        return "The word does not exist. And enter another key."
 
-while True:
 
-    word = input("Enter Key: ")
+word = input("Enter Key or press Enter to end program: ")
 
-    if word == " ":
-        break
+while word != "":
 
+    output = dct(word) ## assign output with returned definition
+
+    if type(output) == list: ## if word has more than one definition print all
+        for item in output:
+            print("Definition: " + item)
+        word = input("Enter Key or press Enter to end program: ")
     else:
-
-        ##dct(word)
-        output = dct(word)
-
-        if type(output) == list:
-            for item in output:
-                print(item)
-        else:
-            print(output)
+        print(output)
+        word = input("Enter Key or press Enter to end program: ")
 
 print("End progam:...")
